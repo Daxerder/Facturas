6561c8ac7e8b83ab11b9ab7b94ffae90483bbfc4
Alex Pinto <alrpintos@gmail.com>
2022-01-01 15:43:28 -0500

    primero

---
+++ app.js
@@ -0,0 +1,27 @@
+const path = require('path')
+const express = require('express')
+const logger = require('morgan')
+const bodyParse = require('body-parser')
+const app = express()
+const indexRoutes = require('./src/routes/index')
+
+
+
+app.set('port', process.env.PORT || 3000 )
+app.set('views', path.join(__dirname, 'src/views'))
+app.set('view engine', 'ejs')
+
+app.use(logger('dev'))
+app.use(express.json())
+app.use(bodyParse.urlencoded({extended:false}))
+app.use('/static', express.static(path.join(__dirname,'src/views/css')))
+app.use('/img', express.static(path.join(__dirname,'src/views/image')))
+app.use('/java', express.static(path.join(__dirname,'src/views/JS')))
+
+app.use('/', indexRoutes)
+
+app.listen(app.get('port'), ()=>{
+    console.log("Servidor en Puerto", app.get('port'))
+})
+
+
